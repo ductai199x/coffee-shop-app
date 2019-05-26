9ed98dfc970e606209899617486d9a289cd09f6d
Tai Nguyen <taiducnguyen.drexel@gmail.com>
2019-05-25 22:00:45 -0400

    initial shop component

---
+++ src/components/Shop.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+class Shop extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+        <div>
+            Example Component
+            <p>{this.props.value}</p>
+        </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        value: state.state.value,
+    }
+}
+
+export default connect(mapStateToProps, null)(Shop);
