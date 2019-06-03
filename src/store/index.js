27d5e631f4e1870e4950ed2d879b3cc4acde3a34
Tai Nguyen <taiducnguyen.drexel@gmail.com>
2019-06-03 19:06:48 -0400

    localstorage now have a lifespan

--- src/store/index.js
+++ src/store/index.js
@@ -12,10 +12,17 @@ const loadState = () => {
     try {
         // Load the data saved in localStorage, against the key 'app_state'
         const serialisedState = window.localStorage.getItem('app_state');
+        const lastAccessedTime = window.localStorage.getItem('last_accessed');
+        const currentTime = Date.now();
+        const expireTime = 18000000;
 
         // Passing undefined to createStore will result in our app getting the default state
         // If no data is saved, return undefined
         if (!serialisedState) return undefined;
+
+        console.log(currentTime - lastAccessedTime);
+
+        if (currentTime - lastAccessedTime > expireTime) return undefined;
 
         // De-serialise the saved state, and return it.
         return JSON.parse(serialisedState);
@@ -28,7 +35,6 @@ const loadState = () => {
 };
 
 const oldState = loadState();
-console.log(oldState)
 export const store = createStore(
     combineReducers({
         shop: shopReducers,
@@ -43,11 +49,11 @@ const saveState = (state) => {
     try {
         // Convert the state to a JSON string 
         const serialisedState = JSON.stringify(state);
-        const currentTime = 
+        const currentTime = Date.now();
 
         // Save the serialised state to localStorage against the key 'app_state'
         window.localStorage.setItem('app_state', serialisedState);
-        window.localStorage.setItem('last_modified', )
+        window.localStorage.setItem('last_accessed', currentTime);
     } catch (err) {
         // Log errors here, or ignore
     }
