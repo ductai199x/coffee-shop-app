84f94daea454345cb84a5aed78ee877fd132fb36
Tai Nguyen <taiducnguyen.drexel@gmail.com>
2019-06-23 20:31:53 -0400

    add firebase to project

---
+++ src/components/Firebase.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app"
+import app from "firebase/app"
+import "firebase/auth"
+import "firebase/firestore"
+import "firebase/database"
+import "firebase/storage"
+import "firebase/functions"
+const config = {
+    apiKey: "AIzaSyBzq-HmsDERmuKCl6-bErS2iojGGA1BmTU",
+    authDomain: "brewlliant-us.firebaseapp.com",
+    databaseURL: "https://brewlliant-us.firebaseio.com",
+    projectId: "brewlliant-us",
+    storageBucket: "brewlliant-us.appspot.com",
+    messagingSenderId: "793719200651",
+    appId: "1:793719200651:web:a6c4cbc83e26128b"
+};
+
+class Firebase {
+    constructor() {
+        app.initializeApp(config);
+    }
+}
+  
+export default Firebase;
